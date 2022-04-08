84b772f5fb9f0986cc68d6403e97370ea5c21ad1
sagar <rebelsagarx@gmail.com>
2022-04-08 11:44:42 +0530

    class lifecycle methods

--- src/App.js
+++ src/App.js
@@ -1,67 +1,25 @@
 import './App.css';
 import React, {useState} from 'react'
+import ClassComponent from './classComponent';
 
-function initialValue(){
-  console.log("function called");
-  return 0;
-}
+
 
 function App() {
-  const [name, setName] = useState("sagar");
   const [flag, setFlag] = useState(false);
-  const [steps, setSteps] = useState(initialValue());
-  const [names, setNames] = useState([])
 
-  let a = "sagar";
-  function changeName(){
-    console.log("clicked");
-    
-    return setFlag(!flag);
-  }
-  function increment(){
-   
-    
-    setSteps((prevState) => prevState + 1);
-    setSteps((prevState) => prevState + 1);
-  }
-  function decrement(){
-   
-    
-    return setSteps(steps - 1);
-  }
 
-  function addNames (e){
-    e.preventDefault();
-    setNames([...names,{id:names.length,name}]);
-    setName("")
-  }
   return (
 
     <div className="App">
-      {/* previously we use class component in order to use state but with the help of the use state react hook , we can actually use the state in our functional component without the converting of of functional component to class component  */}
         
-        <div>Hello. {flag ? name : ""}</div>
-          <button onClick={changeName}>click me</button>
-          <hr />
-        
-          <button onClick={increment}>click me</button>
-          <div>{steps}</div>
-          <button onClick={decrement}>click me</button>
-          <hr />
+        <div>
+          <button onClick={()=>setFlag(!flag)}>Toggle class component</button>
+ 
+        </div>
+       
+{flag ? <ClassComponent/> : ""}
 
-          <form onSubmit={addNames}>
-            <input type="text" value = {name} placeholder="add names" 
-            onChange={(e)=> setName(e.target.value)} 
-
-            />
-            <button>submit</button>
-          </form>
-          <hr />
-          <ul>
-            {(names.map((item)=>
-            <li key={item.id}>{item.name}</li>
-            ))}
-          </ul>
+          
     </div>
   );
 }
--- src/classComponent.js
+++ src/classComponent.js
@@ -1,1 +1,36 @@
-rfce
+import React from 'react'
+
+class ClassComponent extends React.Component {
+
+state = {
+    message: "class component",
+    time: new Date().toString()
+};
+
+componentDidMount(){
+    console.log("i am from did mount");
+    this.interval = setInterval(this.showDate, 1000);
+}
+
+componentDidUpdate() {
+    console.log("i am update");
+}
+
+componentWillUnmount(){
+    console.log("i am from unmount");
+    clearInterval(this.interval)
+}
+
+showDate = ()=>{
+    this.setState({ time: new Date().toString()});
+}
+
+    render(){
+        return (
+            <div>classComponent</div>
+          )
+    }
+
+}
+
+export default ClassComponent
